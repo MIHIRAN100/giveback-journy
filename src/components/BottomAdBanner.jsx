7bf8e6cb9e0960ca15bae641e04f3d4ba83f6c6f
MIHIRAN ARAVINDA <aravindamihiran960@gmail.com>
2026-07-03 17:36:23 +0530

    fix: complete unclosed keyframes block at end of index.css

--- src/components/BottomAdBanner.jsx
+++ src/components/BottomAdBanner.jsx
@@ -11,16 +11,13 @@ const BottomAdBanner = ({ isCookieVisible }) => {
     const bottomPos = isCookieVisible ? '80px' : '0px';
 
     return (
-        <div className="bottom-ad-banner" style={{ bottom: window.innerWidth > 768 ? bottomPos : undefined }}>
+        <div className="bottom-ad-banner" style={{ bottom: bottomPos }}>
             <div className="bottom-ad-inner">
                 <div className="bottom-ad-content">
                     <span className="bottom-ad-tag">LIMITED OFFER</span>
                     <p className="bottom-ad-text">
                         Book your summer journey now and get 15% off on all signature tours! 
                     </p>
-                    <Link to="/packages" className="btn-modern btn-white" style={{ padding: '6px 15px', fontSize: '0.7rem', marginLeft: '10px' }}>
-                        View Deals
-                    </Link>
                 </div>
             </div>
         </div>
